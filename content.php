6fa956f6c89dcb26e1ed89ec42a7e170dac2b60c
mikeyarce <mikeyarce@gmail.com>
2014-12-04 19:15:10 -0800

    updates

--- content.php
+++ content.php
@@ -7,14 +7,6 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 
-		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
-
-		<?php if ( 'post' == get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php aurora_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
-
 		<div class="entry-thumbnail">
 		<?php if ( '' != get_the_post_thumbnail() ) : ?>
 			<?php if ( ! is_single() ) : ?>
@@ -26,6 +18,11 @@
 			<?php endif; ?>
 		<?php endif; ?>
 		</div>
+
+		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+		<?php if ( 'post' == get_post_type() ) : ?>
+		<?php endif; ?>
+
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
