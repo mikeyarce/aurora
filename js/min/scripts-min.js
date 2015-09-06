12ac44f3ea46d909da8d5843a6f00a70b87bbc2a
mikeyarce <mikeyarce@gmail.com>
2015-09-06 10:35:30 -0700

    merging old changes

--- js/min/scripts-min.js
+++ js/min/scripts-min.js
@@ -1,1 +1,1 @@
-jQuery(".sidebar-toggle").click(function(){jQuery(this).toggleClass("open"),jQuery("body").toggleClass("menu-open")});
+jQuery(".sidebar-toggle i").click(function(){jQuery(this).toggleClass("open"),jQuery("body").toggleClass("menu-open")}),jQuery(".page_item_has_children").prepend('<button class="mobile-menu-toggle fa fa-caret-down"></button>'),jQuery(".mobile-menu-toggle").click(function(){jQuery(this).parent(".page_item_has_children").toggleClass("show-children"),jQuery(this).toggleClass("open"),jQuery(this).toggleClass("fa-caret-up")});
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,14 @@
-jQuery('.sidebar-toggle').click(function(){
+/* Toggle Fly-Out Sidebar on top right of site */
+jQuery('.sidebar-toggle i').click(function(){
     jQuery(this).toggleClass('open');
     jQuery('body').toggleClass('menu-open');
+});
+
+/* Toggle Mobile Menu buttons to show child pages */
+jQuery('.page_item_has_children')
+	.prepend('<button class="mobile-menu-toggle fa fa-caret-down"></button>');
+jQuery('.mobile-menu-toggle').click(function() {
+	jQuery(this).parent('.page_item_has_children').toggleClass('show-children');
+	jQuery(this).toggleClass('open');
+	jQuery(this).toggleClass('fa-caret-up');
 });
